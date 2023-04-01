ec9a3cb377f4ea849377d6afa71a4d60cf7faf04
Gabriel Silva <gabriel127.gk@gmail.com>
2023-04-01 14:43:47 -0300

    Ageitando a migração cart_shopper

--- backend/src/database/migrations/20230331154111_cart_shopper.ts
+++ backend/src/database/migrations/20230331154111_cart_shopper.ts
@@ -3,13 +3,13 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("cart_shopper", (table) => {
-    table.integer("owner_id", 255).primary().notNullable();
-    table.integer("id_product_cart", 255).notNullable();
-    table.integer("quantity").notNullable();
+    table.integer("owner_id").primary().notNullable();
+    table.integer("id_product_cart").notNullable();
+    table.integer("quantity_product").notNullable();
+    table.float("total_product_price").notNullable();
     table.foreign("owner_id")
     .references("user_shopper.id_user")
-    .onDelete("CASCADE")
-    .onUpdate("CASCADE");
+    .onDelete("CASCADE");
     table.foreign("id_product_cart")
     .references("product_shopper.id_product")
     .onDelete("CASCADE")
