5b15ac2c2e6a6d14b3e29590121d86054b5c46b0
Gabriel Silva <gabriel127.gk@gmail.com>
2023-05-04 13:19:40 -0300

    Arq. User-login-case - criando lógica - backend

---
+++ backend/src/modules/use-cases/User-cases/User-login-case.ts
@@ -0,0 +1,52 @@
+import { ZodError } from "zod";
+import { UserContract } from "../../repositories/User-contract";
+import { BCryptContract } from "../../../infra/adapters/Bcrypt-contract";
+import { JwtContract } from "../../../infra/adapters/Jwt-contract";
+import { TLoginUserRequest, loginUserSchema } from "./schemas";
+import { UserError } from "../../errors/User-error";
+
+export class UserLoginCase {
+	constructor(
+		private userContract: UserContract,
+		private bcryptContract: BCryptContract,
+		private jwtContract: JwtContract,
+	) {}
+
+	async login(request: TLoginUserRequest) {
+		try {
+			const { email, password } = loginUserSchema.parse(request);
+
+			const user = await this.userContract.findUser({ email });
+
+			if (!user) {
+				throw new UserError("Usuário não encontrado.", 404);
+			}
+
+			const verifyPassword = await this.bcryptContract.compareHash({
+				passwordDatabase: user.password,
+				password,
+			});
+
+			if (!verifyPassword) {
+				throw new UserError("Senha incorreta.", 406);
+			}
+
+      const generateTokenJwt = this.jwtContract.generateToken({
+        userId: user._id,
+        role: user.role,
+      });
+
+      return {
+        statusCode: 200,
+        token: generateTokenJwt,
+      };
+		} catch (error) {
+      if (error instanceof ZodError) {
+        throw new UserError(error.issues[0].message, 406);
+      }
+      if (error instanceof UserError) {
+        throw new UserError(error.message, error.statusCode)
+      }
+    }
+	}
+}
