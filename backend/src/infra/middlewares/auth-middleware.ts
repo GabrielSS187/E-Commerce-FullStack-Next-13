49a878ad650cf439540eb7d61ffd003890ec089f
Gabriel Silva <gabriel127.gk@gmail.com>
2023-05-07 17:03:53 -0300

    Criando lógica do arq. auth-middleware.ts - backend

---
+++ backend/src/infra/middlewares/auth-middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+
+import { JwtAdapter } from "../adapters/JwtAdapter/Jwt-adapter";
+import { CustomError } from "../../modules/errors/CustomError";
+import { JsonWebTokenError } from "jsonwebtoken";
+
+const jwt = new JwtAdapter();
+
+export const authMiddleware = (
+	req: Request,
+	res: Response,
+	nex: NextFunction,
+) => {
+	try {
+		const { authorization } = req.headers;
+
+		if (!authorization) {
+			throw new CustomError("Não autorizado, o token é necessário", 401);
+		}
+
+		const token = authorization.replace("Bearer", "").trim();
+
+		const decoded = jwt.getToken({ token });
+		req.idUser = decoded.userId;
+		req.userRole = decoded.role;
+
+		nex();
+	} catch (error) {
+		if (error instanceof JsonWebTokenError) {
+			throw new CustomError(error.message, 401);
+		}
+		if (error instanceof CustomError) {
+			throw new CustomError(error.message, error.statusCode);
+		}
+	}
+};
