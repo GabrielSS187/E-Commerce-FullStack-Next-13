f038d202b614eb75aea308701a739e3b70072d07
Gabriel Silva <gabriel127.gk@gmail.com>
2023-05-08 15:58:48 -0300

    Teste arq. delete-user-case.test.ts - teste-2 - backend

--- backend/src/modules/tests/unitary/use-cases/User-cases/delete-user-case.test.ts
+++ backend/src/modules/tests/unitary/use-cases/User-cases/delete-user-case.test.ts
@@ -3,6 +3,7 @@ import { expect, describe, it, vi } from "vitest";
 import { usersDbMock } from "../../../database-in-memory/database-mock";
 import { UserRepositoryInMemory } from "../../../repositories-in-memory/User-repository-in-memory";
 import { DeleteUserCase } from "../../../../use-cases/User-cases/Delete-user-case";
+import { UserError } from "../../../../errors/User-error";
 
 const sutFactory = () => {
 	const userRepository = new UserRepositoryInMemory();
@@ -26,5 +27,21 @@ describe("Test in the file delete-user-case", () => {
 		expect(usersDbMock).toHaveLength(2);
 
 		expect.assertions(2);
+	});
+
+	it("should throw an error if the user doesn't exist.", async () => {
+		try {
+			await sutDeleteUserCase.delete({ idUser: "id_not_exist" });
+			throw new Error("Test failed");
+			// rome-ignore lint/suspicious/noExplicitAny: <explanation>
+		} catch (error: any) {
+      expect(error).instanceOf(UserError);
+      expect(error.message).toBe("Usuário não encontrado.");
+      expect(error.statusCode).toBe(404);
+    }
+
+    expect(usersDbMock).toHaveLength(2);
+
+    expect.assertions(4);
 	});
 });
