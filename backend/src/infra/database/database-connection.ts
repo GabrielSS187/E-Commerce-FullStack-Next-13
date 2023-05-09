818e46008872c4d6e5bf74b3ab7c5aeb640e53a2
Gabriel Silva <gabriel127.gk@gmail.com>
2023-05-09 02:54:29 -0300

    Criando arq. de conexão => database-connection.ts - v2 - backend

--- backend/src/infra/database/database-connection.ts
+++ backend/src/infra/database/database-connection.ts
@@ -12,13 +12,13 @@ export const databaseConnection = async (): Promise<void> => {
 		if (env.NODE_ENV === "production") {
 			// rome-ignore lint/style/noNonNullAssertion: <explanation>
 			await mongoose.connect(env.DATABASE_URL!);
-			loggerInstance.info("successful connection to mongoDB => P 🎉");
+			loggerInstance.info("successful connection to mongoDB => P");
 		}
 
 		if (env.NODE_ENV === "development") {
 			// rome-ignore lint/style/noNonNullAssertion: <explanation>
 			await mongoose.connect(env.DATABASE_URL!);
-			loggerInstance.info("successful connection to mongoDB => D 🎉");
+			loggerInstance.info("successful connection to mongoDB => D");
 		}
 		// rome-ignore lint/suspicious/noExplicitAny: <explanation>
 	} catch (error: any) {
